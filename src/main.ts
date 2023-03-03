916b539e7e9aaef45417a2713419757a60958843
odoe <odoe@odoe.net>
2023-03-03 13:00:04 -0800

    feat: add map

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  </div>
-`
+import ArcGISMap from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
+const layer = new FeatureLayer({
+    portalItem: {
+        id: "f9e348953b3848ec8b69964d5bceae02"
+    },
+    outFields: ["SEASON"]
+});
+
+const map = new ArcGISMap({
+    basemap: "gray-vector",
+    layers: [layer]
+});
+
+const view = new MapView({
+    map,
+    container: "viewDiv",
+    center: [-98, 40],
+    zoom: 4
+})
